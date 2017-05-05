fbe8db2d03462daec960b1226744f853f17723b4
Will Humphreys <whumphreys@gmail.com>
2017-05-05 22:39:43 +0200

    Add a couple of Spring Integration tests
    Should of done this earlier. :(

--- src/test/java/com/goeuro/challenges/BusRouteChallengeApplicationTests.java
+++ src/test/java/com/goeuro/challenges/BusRouteChallengeApplicationTests.java
@@ -2,15 +2,33 @@ package com.goeuro.challenges;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@TestPropertySource(properties = {"data.location = data/example"})
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BusRouteChallengeApplicationTests {
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+
 	@Test
-	public void contextLoads() {
+	public void shouldReturnFalseIfNoDirectRoute() {
+		String body = this.restTemplate.getForObject("/api/direct?dep_sid=150&arr_sid=152", String.class);
+		assertThat(body, is(equalTo("{\"departureSid\":150,\"arrivalSid\":152,\"directBusRoute\":false}")));
 	}
 
+	@Test
+	public void shouldReturnTrueIfDirectRoute() {
+		String body = this.restTemplate.getForObject("/api/direct?dep_sid=150&arr_sid=153", String.class);
+		assertThat(body, is(equalTo("{\"departureSid\":150,\"arrivalSid\":153,\"directBusRoute\":true}")));
+	}
 }
