1dbdedc98f71ff05cf8ff8a5cdbc562017587a0b
Will Humphreys <whumphreys@gmail.com>
2017-05-05 22:12:01 +0200

    Initialize the collection sizes to sensible defaults

--- src/main/java/com/goeuro/challenges/data/StopsAndRoutes.java
+++ src/main/java/com/goeuro/challenges/data/StopsAndRoutes.java
@@ -3,13 +3,20 @@ package com.goeuro.challenges.data;
 import java.util.*;
 
 class StopsAndRoutes {
-    private Map<Integer, Set<Integer>> stopRoutesMap = new HashMap<>();
+    private static final int NUMBER_OF_STATIONS = 1_000_000;
+    private static final int NUMBER_OF_BUS_ROUTES = 100_000;
+    private static final int MAX_STOPS_ON_A_ROUTE = 1000;
+    private int AVERAGE_NUMBER_OF_ROUTES_PER_STOP =
+            NUMBER_OF_BUS_ROUTES * MAX_STOPS_ON_A_ROUTE / NUMBER_OF_STATIONS;
+
+    private Map<Integer, Set<Integer>> stopRoutesMap = new HashMap<>(NUMBER_OF_STATIONS);
 
     void add(RouteWithStops routeWithStops) {
         Integer route = routeWithStops.getRoute();
         Set<Integer> stops = routeWithStops.getStops();
         stops.forEach(stop -> {
-            Set<Integer> routes = stopRoutesMap.computeIfAbsent(stop, k -> new HashSet<>());
+            Set<Integer> routes = stopRoutesMap.computeIfAbsent(
+                    stop, k -> new HashSet<>(AVERAGE_NUMBER_OF_ROUTES_PER_STOP));
             routes.add(route);
         });
     }
